34eaf6c97cf7ca5b1600d83fbc8398e9cd0c7d75
Raqhib <raqhib.ataillah@gmail.com>
2025-05-18 13:54:57 +0800

    connect Network API, fetching API Reg

--- Brainy/app/src/main/java/id/co/brainy/data/repository/AuthRepository.kt
+++ Brainy/app/src/main/java/id/co/brainy/data/repository/AuthRepository.kt
@@ -1,5 +1,42 @@
 package id.co.brainy.data.repository
 
-class AuthRepository (){
+import id.co.brainy.data.model.LoginReq
+import id.co.brainy.data.model.RegisterReq
+import id.co.brainy.data.network.response.LoginResponse
+import id.co.brainy.data.network.response.RegistResponse
+import id.co.brainy.data.network.retrofit.ApiService
+import id.co.brainy.data.utils.parseErrorMessage
+
+class AuthRepository(private val apiService: ApiService){
+
+    suspend fun register(
+        username: String,
+        email: String,
+        password: String
+    ): Result<RegistResponse>{
+        return try {
+            val request = RegisterReq(username,email,password)
+            val response = apiService.register(request)
+            Result.success(response)
+        }catch (e: Exception){
+            val message = parseErrorMessage(e)
+            Result.failure(Exception(message))
+        }
+    }
+
+    suspend fun login(
+        email: String,
+        password: String
+    ): Result<LoginResponse>{
+        return try {
+            val request = LoginReq(email,password)
+            val response = apiService.login(request)
+            Result.success(response)
+        }catch (e: Exception){
+            val message = parseErrorMessage(e)
+            Result.failure(Exception(message))
+        }
+    }
+
 
 }
--- Brainy/app/src/main/java/id/co/brainy/ui/ViewModelFactory.kt
+++ Brainy/app/src/main/java/id/co/brainy/ui/ViewModelFactory.kt
@@ -1,4 +1,28 @@
 package id.co.brainy.ui
 
-class ViewModelFactory {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import id.co.brainy.data.network.retrofit.ApiConfig
+import id.co.brainy.data.repository.AuthRepository
+import id.co.brainy.ui.screen.auth.AuthViewModel
+
+class ViewModelFactory(private val context: Context) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        val apiService = ApiConfig.getApiService()
+
+        return when {
+            modelClass.isAssignableFrom(AuthViewModel::class.java) -> {
+                AuthViewModel(AuthRepository((apiService))) as T
+            }
+
+            else -> throw IllegalArgumentException("Unknown viewmodel class: " + modelClass.name)
+        }
+
+
+    }
+
 }
